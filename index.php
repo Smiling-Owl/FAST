5456c37dbf3bc1ccb690f62334375f3982fd8bab
Sai010ritso <129629143+Sai010ritso@users.noreply.github.com>
2025-05-03 18:10:23 +0800

    Update icon index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Foundation of Ateneo Student Tutors</title>
-    <link rel="icon" type="image/x-icon" href="/Main-images/FAST logo white trans.png">
+    <link rel="icon" type="image/x-icon" href="Main-images/FAST logo white trans.png">
     <link rel="stylesheet" href="styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
